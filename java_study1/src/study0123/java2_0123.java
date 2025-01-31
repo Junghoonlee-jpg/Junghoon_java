50ff409b5d13e6e474cb2f2dd761cddc99a11261
Junghoonlee-jpg <ljh2ljh2@naver.com>
2025-01-31 12:54:08 +0900

    20250131

--- java_study1/src/study0123/java2_0123.java
+++ java_study1/src/study0123/java2_0123.java
@@ -7,6 +7,12 @@ public class java2_0123 {
 		System.out.println( 27 & 13); // and 연산
 		System.out.println( 27 | 13); // or 연산
 		System.out.println( 27 ^ 13); // xor 연산
+		System.out.println( ~-13); // not 연산
+		
+		// 비트 쉬프트 연산자
+		System.out.println( 21 << 2); // << 시프트 연산자
+		System.out.println( 17 >> 3); // >> 시프트 연산자
+		
 
 	}
 
